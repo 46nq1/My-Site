494d16090de78d9f82ed6f4ea115bc0613f4f107
46nq1 <leoh000497@gmail.com>
2024-12-22 18:50:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "socialmedia.html"; // Redirects to another page
+});
+
+document.getElementById("anotherButton").addEventListener("click", function() {
+    href="https://www.youtube.com/@46nq1Roblox"
+});
